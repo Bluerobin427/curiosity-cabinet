33c3ad45769e2ec381bc538a14773ff1270469cc
Bluerobin427 <robinmcp@gmail.com>
2025-07-21 20:38:47 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+async function retrieveNotes() {
+  const requestURL =
+    "notes.json";
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const notes = await response.json();
+
+  populateNotes(notes);
+}
+
+function populateNotes(notes){
+    const section = document.querySelector("#notesArea");
+    
+    for (const note of notes) {
+        const nextNote = document.createElement("article");
+        const noteH3 = document.createElement("h3");
+        const noteP = document.createElement("p");
+
+        noteH3.textContent = note.title;
+        noteP.textContent = note.content;
+
+        nextNote.appendChild(noteH3);
+        nextNote.appendChild(noteP);
+
+        section.appendChild(nextNote);
+    }
+}
+
+retrieveNotes();
