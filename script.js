b3a45a051586cb21f3b2187db7d91a76d95940a2
Bluerobin427 <robinmcp@gmail.com>
2025-07-25 15:19:16 -0400

    Added edit function
    Added the edit function. Also created spans to store the ID of a note being edited and to store the mode that the form is in (currently "create" and "edit" are used.
    
    Changed the form type to post instead of submit.
    
    Got "preventDefault" to work on the form, by putting it up at the top of the function and passing the event through the function. (see "e" in the saveNote function).
    
    Added some commenting, need to add more. Cleaned up some old console logging and comments.

--- script.js
+++ script.js
@@ -27,28 +27,51 @@ async function retrieveNotes(){
 }
 */
 
-function saveNote(){
+function saveNote(e){
+  e.preventDefault();
+
+  //Grab the HTML elements and their contents, then create the note.
   const name = document.getElementById("NoteName").value;
-  const noteID = `note-${Date.now()}`;
   const content = document.getElementById("NoteContent").value;
   const button = document.getElementById("SubmitButton");
+  const formMode = document.getElementsByClassName("formMode")[0];
+  let noteId = "";
 
-  let note = {"noteID":noteID, "name":name, "content":content};
-  //const note = name.value;
+  if (formMode.id == "edit") {
+    noteId = document.getElementsByClassName("noteIdHolder")[0].id;
+  } else if (formMode.id == "create"){
+    noteId = `note-${Date.now()}`;
+    console.log("making a new note!");
+  } else {
+    alert("Something went wrong with the form mode!")
+  }
 
+  let newNote = {"noteID":noteId, "name":name, "content":content};
+
+  //Grab the noteLibrary and add the note.
+  //If the note already exists, replace it with the edited one.
   let noteLibrary = getNoteLibrary();
-  noteLibrary.push(note);
-  console.log(noteLibrary);
+  if (formMode.id == "edit") {
+    //find the index
+    const index = noteLibrary.findIndex((entry) => entry.noteID === newNote.noteID);
+    //edit the note
+    noteLibrary[index] = newNote;
+  } else if (formMode.id == "create"){
+    noteLibrary.push(newNote);
+  } 
+
   setNoteLibrary(noteLibrary);
 
+  //Make the button let you know it worked.
   button.textContent = "Note Saved!";
+  formMode.id = "create";
 
+  //Wrap up and clean up.
   console.log("Save Ran");
-  event.preventDefault();
   
   setTimeout(() => {
     form.submit();
-  }, 2000);
+  }, 1000);
 }
 
 function populateNotes(){
@@ -61,15 +84,14 @@ function populateNotes(){
         const nextNote = document.createElement("article");
         const noteH3 = document.createElement("h3");
         const noteP = document.createElement("p");
+        const noteEditButton = document.createElement("button");
 
         noteH3.textContent = note.name;
+        noteEditButton.textContent = "✏️";
+        noteEditButton.id = note.noteID;
+        noteEditButton.className = "editButton";
+        noteH3.appendChild(noteEditButton);
 
-        //console.log(noteContent);
-        //if (noteContent.includes('\n')){
-        //  noteContent.replace(/(\r\n|\r|\n)/g, '\r\n');
-        //  console.log("Found a newline!");
-        //}
-        //console.log(noteContent);
         noteP.textContent = note.content;
         nextNote.appendChild(noteH3);
         nextNote.appendChild(noteP);
@@ -78,13 +100,59 @@ function populateNotes(){
     }
 }
 
+//The next functions handle when happens when you click on
+//the edit and delete buttons on notes.
+function handleClick(e) {
+  if (e.target.className == "editButton"){
+    editNote(e);
+  }
+  else if (e.target.className == "deleteButton"){
+    deleteNote(e);
+  }
+  else {
+    return false;
+  }
+}
+function editNote(e){
+  //Grab the note's noteID from the button (it's the button's ID)
+  let editingNoteID = e.target.id;
+
+  //Grab the note editing form's HTML elements.
+  const formName = document.getElementById("NoteName");
+  const formContent = document.getElementById("NoteContent");
+  const formIdHolder = document.getElementsByClassName("noteIdHolder")[0];
+  const formButton = document.getElementById("SubmitButton");
+  const formMode = document.getElementsByClassName("formMode")[0];
+
+  //Grab the noteLibrary.
+  let noteLibrary = getNoteLibrary();
+
+  //Find the note
+  const existingNote = noteLibrary.find((entry) => entry.noteID === editingNoteID);
+  //When you find it, set the note editing form's text fields
+  //to the notes's name and content.
+  formName.value = existingNote.name;
+  formContent.value = existingNote.content;
+  formIdHolder.id = existingNote.noteID;
+  formButton.textContent = "Edit Note";
+  formMode.id = "edit";
+}
+function deleteNote(e){
+  //nothing here yet.
+}
+
+//If the noteLibrary doesn't exist, create it.
 if(localStorage.noteLibrary == 'undefined'){
   createNoteLibrary();
 }
 
+//Event Listeners! One for submitting the note editing form and one
+//for handling clicks on edit and delete buttons (calls functions above)
 const form = document.getElementById("form");
 document.addEventListener('DOMContentLoaded', function() {
   form.addEventListener("submit", saveNote);
+  document.body.addEventListener("click", handleClick);
 });
 
+//Populate the notes area.
 populateNotes();
